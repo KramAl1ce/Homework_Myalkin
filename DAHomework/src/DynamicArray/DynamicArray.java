587fb550880878afb3f11efca56dd45d9e170b44
ViktoriaMois <myalkin.stepan@mail.ru>
2021-03-27 22:56:10 +0300

    Task 1&2 completed
    added all task 2 functions and tests for them

--- DAHomework/src/DynamicArray/DynamicArray.java
+++ DAHomework/src/DynamicArray/DynamicArray.java
@@ -5,8 +5,12 @@ public class DynamicArray<A> {
     public int size = 0;
 
     public static String outOfBorderError = "Выход за границы массива";
+    public static String cantFindElementError = "Данного элемента не существует";
 
-    public DynamicArray() {
+    public DynamicArray(int actSize) {
+        if (actSize < 1) {
+            throw new RuntimeException(outOfBorderError);
+        }
         array = (A[]) new Object[size];
     }
 
@@ -38,5 +42,55 @@ public class DynamicArray<A> {
         return array.length;
     }
 
+    public void addData(A value) {                    //Task 2 complete
+        resize(array.length + 1);
+        array[array.length - 1] = value;
+    }
 
+    public int findFirst(A value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        throw new RuntimeException(cantFindElementError);
+    }
+
+    public int findLast(A value) {
+        int lastind = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                lastind = i;
+            }
+        }
+        if (lastind >= 0) {
+            return lastind;
+        }
+        throw new RuntimeException(cantFindElementError);
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index > array.length) {
+            throw new RuntimeException(outOfBorderError);
+        }
+        A[] newAr = (A[]) new Object[array.length - 1];
+        for (int i = 0; i < index; i++) {
+            newAr[i] = array[i];
+        }
+        for (int i = index + 1; i < array.length; i++) {
+            newAr[i - 1] = array[i];
+        }
+        array = newAr;
+    }
+
+    public void insert(int index, A value) {
+        if (index < 0 || index > array.length) {
+            throw new RuntimeException(outOfBorderError);
+        }
+        resize(array.length + 1);
+        for (int i = array.length - 1; i > index; i--) {
+            array[i] = array[i-1];
+        }
+        setData(index, value);
+    }
 }
--- DAHomework/src/DynamicArray/DynamicArrayTests.java
+++ DAHomework/src/DynamicArray/DynamicArrayTests.java
@@ -1,4 +1,31 @@
 package DynamicArray;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DynamicArrayTests {
+    @Test
+    public void init_sizeDefault() {
+        DynamicArray array = new DynamicArray(10);
+        assertEquals(array.size, array.getSize());
+    }
+
+    @Test
+    public void testSize(){
+        DynamicArray array = new DynamicArray(25);
+        assertEquals(25, array.getSize());
+    }
+
+    @Test
+    public void testGetSize(){
+        DynamicArray array = new DynamicArray(45);
+        assertEquals(45, array.array.length);
+    }
+
+    @Test
+    public void testAddData(){
+        DynamicArray array = new DynamicArray(1);
+        array.addData(345);
+        assertEquals(345, array.getData(1));
+    }
 }
