bf8b5813cb8930d04e6cf5fa9fa835c7ab8b9a54
ViktoriaMois <myalkin.stepan@mail.ru>
2021-03-27 22:01:40 +0300

    Added addData, getData and getSize functions
    added 3 functions and a lil fix of resize

--- DAHomework/src/DynamicArray/DynamicArray.java
+++ DAHomework/src/DynamicArray/DynamicArray.java
@@ -4,12 +4,14 @@ public class DynamicArray<A> {
     public A[] array;
     public int size = 0;
 
+    public static String outOfBorderError = "Выход за границы массива";
+
     public DynamicArray() {
         array = (A[]) new Object[size];
     }
 
     public void resize(int actSize) {
-        A[] newArray = (A[]) new Object[actSize]);
+        A[] newArray = (A[]) new Object[actSize];
         int a = actSize;
         if (actSize > array.length) {
             a = array.length;
@@ -17,4 +19,24 @@ public class DynamicArray<A> {
         System.arraycopy(array, 0, newArray, 0, a);
         array = newArray;
     }
+
+    public A getData(int number) {
+        if (array.length < number || number < 0) {
+            throw new RuntimeException(outOfBorderError);
+        }
+        return array[number];
+    }
+
+    public void setData(int number, A value) {
+        if (number < 0 || number > array.length) {
+            throw new RuntimeException(outOfBorderError);
+        }
+        array[number] = value;
+    }
+
+    public int getSize() {
+        return array.length;
+    }
+
+
 }
