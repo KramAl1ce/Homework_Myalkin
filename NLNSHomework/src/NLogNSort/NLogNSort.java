f7d658a5fe342ebe2329891bb3a78fb8c9571504
KramA1lce <myalkin.stepan@mail.ru>
2021-04-09 02:25:53 +0300

    Update on NLogNSort
    Added method of sorting found on some site. It had seen some horror in the past but hope it works

--- NLNSHomework/src/NLogNSort/NLogNSort.java
+++ NLNSHomework/src/NLogNSort/NLogNSort.java
@@ -3,8 +3,29 @@ package NLogNSort;
 import java.util.Arrays;
 
 public class NLogNSort {
+    private static void merge(int[] hdata, int a, int p, int q) {
+        int e = q - a + 1;
+        int[] Array = new int[e];
+
+        int i = a;
+        int j = p + 1;
+        int k = 0;
+
+        while (i <= p || j <= q) {
+            if (i > p)
+                Array[k++] = hdata[j++];
+            else if (j > q)
+                Array[k++] = hdata[i++];
+            else if (hdata[i] < hdata[j])
+                Array[k++] = hdata[i++];
+            else
+                Array[k++] = hdata[j++];
+        }
+        for (j = 0; j < e; j++)
+            hdata[a + j] = Array[j];
+    }
+
     public void sort(int[] data)
     {
-        Arrays.sort(data); // Replace this
     }
 }
--- NLNSHomework/src/NLogNSort/NLogNSortTest.java
+++ NLNSHomework/src/NLogNSort/NLogNSortTest.java
@@ -1,3 +1,5 @@
+package NLogNSort;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
