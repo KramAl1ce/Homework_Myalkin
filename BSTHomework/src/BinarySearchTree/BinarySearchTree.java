a2fa2e6cb5bf920180a8ebecbd245b6be0f1f471
KramA1lce <myalkin.stepan@mail.ru>
2021-05-04 20:37:35 +0300

    Task 9 complete

--- BSTHomework/src/BinarySearchTree/BinarySearchTree.java
+++ BSTHomework/src/BinarySearchTree/BinarySearchTree.java
@@ -18,6 +18,10 @@ public class BinarySearchTree {
         }
     }
 
+    public Node search(String key) {
+        return searchRec(key, root);
+    }
+
     public Node getSuccessor(Node thisNode) {
         return getSuccessorRec(thisNode);
     }
@@ -51,6 +55,49 @@ public class BinarySearchTree {
 //        }
 //        return null;
 //    }
+    public void delete(Node thisNode, Node thisNodeParent, Node thisNodeChild) {
+        Node HeadNode = deleteRec(thisNode, thisNodeParent, thisNodeChild);
+    }
+
+    public Node getMin() {
+        Node thisNode = root;
+        while (nodeExists(thisNode, "left")) {
+            thisNode = thisNode.leftNode;
+        }
+
+        return thisNode;
+    }
+
+    public Node getMax() {
+        Node thisNode = root;
+        while (nodeExists(thisNode, "right")) {
+            thisNode = thisNode.rightNode;
+        }
+
+        return thisNode;
+    }
+
+    private static Node insertRec(String key, Node thisNode) {
+        if (key.compareTo(thisNode.key) > 0) {
+            if (thisNode.rightNode == null) {
+                return insertRec(key, thisNode.rightNode);
+            } else {
+                thisNode.rightNode = new Node(key);
+                thisNode.rightNode.parNode = thisNode;
+                return thisNode.rightNode;
+            }
+        } else if (key.compareTo(thisNode.key) <= 0) {
+            if (thisNode.leftNode != null) {
+                return insertRec(key, thisNode.leftNode);
+
+            } else {
+                thisNode.leftNode = new Node(key);
+                thisNode.leftNode.parNode = thisNode;
+                return thisNode.leftNode;
+            }
+        }
+        return null;
+    }
 
     private Node deleteRec(Node thisNode, Node thisNodeParent, Node thisNodeChild) {
         thisNodeParent = getPredecessor(thisNode);
@@ -78,27 +125,39 @@ public class BinarySearchTree {
             }
 
             thisNodeSuccessor.parNode = focusNodeParent;
+            thisNodeSuccessor.parNode = thisNodeParent;
             return thisNodeSuccessor;
 
         }
 
         else if (countChildren(focusNode) == 1) {
+        else if (countChildren(thisNode) == 1) {
+
+            if (nodeExists(thisNode, "left")) {
 
             if (nodeExists(focusNode, "left")) {
                 // if the node only has left child
 
                 Node newChild = focusNode.leftNode; // copy new child
                 focusNode.leftNode.parentNode = null; // delete focusNode from memory
+                Node newChild = thisNode.leftNode;
+                thisNode.leftNode.parNode = null;
                 if (isLeftChild) {
                     focusNodeParent.leftNode = newChild; // link parent to child
+                    thisNodeParent.leftNode = newChild;
 
                     newChild.parentNode = focusNodeParent; // link child to parent
                     return focusNodeParent.leftNode;
+                    newChild.parNode = thisNodeParent;
+                    return thisNodeParent.leftNode;
                 } else {
                     focusNodeParent.rightNode = newChild;
+                    thisNodeParent.rightNode = newChild;
 
                     newChild.parentNode = focusNodeParent; // link child to parent
                     return focusNodeParent.rightNode;
+                    newChild.parNode = thisNodeParent;
+                    return thisNodeParent.rightNode;
                 }
 
             } else {
@@ -106,35 +165,63 @@ public class BinarySearchTree {
 
                 Node newChild = focusNode.rightNode;
                 focusNode.rightNode.parentNode = null;
+                Node newChild = thisNode.rightNode;
+                thisNode.rightNode.parNode = null;
                 if (isLeftChild) {
                     focusNodeParent.leftNode = newChild;
+                    thisNodeParent.leftNode = newChild;
 
                     newChild.parentNode = focusNodeParent;
                     return focusNodeParent.leftNode;
+                    newChild.parNode = thisNodeParent;
+                    return thisNodeParent.leftNode;
                 } else {
                     focusNodeParent.rightNode = newChild;
+                    thisNodeParent.rightNode = newChild;
 
                     newChild.parentNode = focusNodeParent;
                     return focusNodeParent.rightNode;
+                    newChild.parNode = thisNodeParent;
+                    return thisNodeParent.rightNode;
                 }
             }
         }
 
         else if (countChildren(focusNode) == 0) {
+        else if (countChildren(thisNode) == 0) {
             if (isLeftChild) {
                 focusNodeParent.leftNode = null;
                 return focusNodeParent;
+                thisNodeParent.leftNode = null;
+                return thisNodeParent;
             } else {
                 focusNodeParent.rightNode = null;
                 return focusNodeParent;
+                thisNodeParent.rightNode = null;
+                return thisNodeParent;
             }
         }
 
         return null;
     }
 
-}
+    private static Node searchRec(String key, Node thisNode) {
+        if (thisNode == null) {
+            return null;
+        }
+        if (key.equals(thisNode.key)) {
+            return thisNode;
+        }
+        if (key.compareTo(thisNode.key) > 0) {
+            return searchRec(key, thisNode.rightNode);
+        }
 
+        else if (key.compareTo(thisNode.key) < 0) {
+            return searchRec(key, thisNode.leftNode);
+        }
+        return null;
+    }
+    
     private static Node getSuccessorRec(Node thisNode) {
         if (nodeExists(thisNode, "right")) {
             thisNode = thisNode.rightNode;
@@ -147,8 +234,12 @@ public class BinarySearchTree {
                 Node focusNodeParent = thisNode.parNode;
                 if (focusNodeParent.leftNode == thisNode) {
                     return focusNodeParent;
+                Node thisNodeParent = thisNode.parNode;
+                if (thisNodeParent.leftNode == thisNode) {
+                    return thisNodeParent;
                 } else {
                     thisNode = focusNodeParent;
+                    thisNode = thisNodeParent;
                 }
             }
             return null;
@@ -167,10 +258,14 @@ public class BinarySearchTree {
             while (nodeExists(thisNode, "parent")) {
                 Node focusNodeParent = thisNode.parNode;
                 if (focusNodeParent.rightNode == thisNode) {
+                Node thisNodeParent = thisNode.parNode;
+                if (thisNodeParent.rightNode == thisNode) {
 
                     return focusNodeParent;
+                    return thisNodeParent;
                 } else {
                     thisNode = focusNodeParent;
+                    thisNode = thisNodeParent;
                 }
             }
             return null;
@@ -178,12 +273,15 @@ public class BinarySearchTree {
     }
 
     private static int countChildren(Node focusNode) {
+    private static int countChildren(Node thisNode) {
         int count = 0;
         if (nodeExists(focusNode, "left")) {
+        if (nodeExists(thisNode, "left")) {
             count++;
         }
 
         if (nodeExists(focusNode, "right")) {
+        if (nodeExists(thisNode, "right")) {
             count++;
         }
         return count;
